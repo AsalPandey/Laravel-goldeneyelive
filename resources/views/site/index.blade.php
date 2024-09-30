d6d1380e12a85d8ae4d721254b5cc398e1561f31
Prasun Paudel <prasunpaudel2001@gmail.com>
2024-09-30 17:24:02 +0545

    team photo made responsive

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -300,73 +300,49 @@
                 <h6 class="section-title bg-white text-center text-primary px-3">Instructors</h6>
                 <h1 class="mb-5">Expert Instructors</h1>
             </div>
-            <div class="row g-4">
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="row g-4 justify-content-center">
+                <!-- Instructor 1 -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" style="height:300px;width:300px" src="{{ asset("site/img/user.png") }}" alt="">
+                            <img class="img-fluid" style="height:300px;width:100%; object-fit: cover;" src="{{ asset('site/img/user.png') }}" alt="Instructor 1">
                         </div>
-                        {{-- <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div> --}}
                         <div class="text-center p-4">
-                            <h5 class="mb-0">Aakash Shubedi</h5></h5>
+                            <h5 class="mb-0">Aakash Shubedi</h5>
                             <small>IELTS/PTE/TOFEL Instructor</small>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                <!-- Instructor 2 -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center wow fadeInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" style="height:300px;width:300px" src="{{ asset("site/img/team-2.jpg") }}" alt="">
+                            <img class="img-fluid" style="height:300px;width:100%; object-fit: cover;" src="{{ asset('site/img/team-2.jpg') }}" alt="Instructor 2">
                         </div>
-                        {{-- <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div> --}}
                         <div class="text-center p-4">
                             <h5 class="mb-0">Pradeep Paudel</h5>
                             <small>Korean Teacher</small>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                <!-- Instructor 3 -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center wow fadeInUp" data-wow-delay="0.5s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" style="height:300px;width:300px" src="{{ asset("site/img/team-3.jpg") }}" alt="">
+                            <img class="img-fluid" style="height:300px;width:100%; object-fit: cover;" src="{{ asset('site/img/team-3.jpg') }}" alt="Instructor 3">
                         </div>
-                        {{-- <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div> --}}
                         <div class="text-center p-4">
                             <h5 class="mb-0">Surendra Bhattarai</h5>
                             <small>Computer Teacher</small>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                <!-- Instructor 4 -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center wow fadeInUp" data-wow-delay="0.7s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" style="height:300px;width:300px" src="{{ asset("site/img/team_1.jpg") }}" alt="">
+                            <img class="img-fluid" style="height:300px;width:100%; object-fit: cover;" src="{{ asset('site/img/team_1.jpg') }}" alt="Instructor 4">
                         </div>
-                        {{-- <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
-                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div> --}}
                         <div class="text-center p-4">
                             <h5 class="mb-0">Ajay B.k</h5>
                             <small>English Teacher</small>
@@ -377,6 +353,7 @@
         </div>
     </div>
     <!-- Team End -->
+
 
 
     <!-- Testimonial Start -->
